632a8f47536e69764f18e02ca4eea51d6219f7ee
Narayan Agarwal <narayanrec@gmail.com>
2025-10-04 20:45:01 -0500

    Add login and logout functionality

---
+++ auth.ts
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import type { User } from "@/app/lib/definitions";
+import bcrypt from "bcrypt";
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await fetch(
+      `http://localhost:8080/api/users/by-email?email=${email}`
+    ).then((res) => res.json());
+
+    if (user == null) return undefined;
+    return user;
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+        console.log("Parsed Credentials:", parsedCredentials);
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+
+          const user = await getUser(email);
+          console.log("Fetched User:", user);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+        console.log("Invalid credentials");
+        return null;
+      },
+    }),
+  ],
+});
